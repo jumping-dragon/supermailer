3cdbab06b55dfc83ecc93538233c402a3c0c5d46
Alvin Januar <alvin.januar@ecloudvalley.com>
2024-03-20 23:49:37 +0800

    feat: add inbox lambda infra

--- infra/email-setup.tf
+++ infra/email-setup.tf
@@ -9,6 +9,12 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  default_tags {
+    tags = {
+      Owner   = "Alvin"
+      Project = "Supermailer"
+    }
+  }
 }
 
 variable "zone_id" {
@@ -107,15 +113,21 @@ resource "aws_ses_receipt_rule" "catch-all" {
     position    = 1
   }
 
+  lambda_action {
+    function_arn = "${aws_lambda_function.inbox_lambda.arn}"
+    invocation_type = "Event"
+    position = 2
+  }
+
   stop_action {
     scope    = "RuleSet"
-    position = 2
+    position = 3
   }
 
   depends_on = [
     aws_s3_bucket.mail-bucket,
     aws_s3_bucket_policy.mail-bucket-policy,
-    aws_sns_topic.mail-topic
+    aws_sns_topic.mail-topic,
   ]
 }
 
@@ -197,9 +209,9 @@ resource "aws_ses_event_destination" "ses_cloudwatch" {
 }
 
 data "archive_file" "api_server_zip" {                                                                                                                                                                                   
-  type        = "zip"                                                                                                                                                                                                
-  source_dir  = "../target/lambda/mailer"                                                                                                                                                                                         
-  output_path = "./terraform-handler.zip"                                                                                                                                                                         
+  type        = "zip"                                                                                                                                                                                                                                         
+  source_dir  = "../target/lambda/supermailer"                                                                                                                                                                                         
+  output_path = "./api_server.zip"                                                                                                                                                                         
 } 
 
 resource "aws_lambda_function" "api_server" {
@@ -260,4 +272,72 @@ resource "aws_iam_policy" "api_server_policy" {
 resource "aws_iam_role_policy_attachment" "api_server_policy_role_attachment" {
   role       = aws_iam_role.api_server_role.name
   policy_arn = aws_iam_policy.api_server_policy.arn
+}
+
+data "archive_file" "inbox_lambda_zip" {                                                                                                                                                                                   
+  type        = "zip"                                                                                                                                                                                                                                                                
+  source_dir  = "../target/lambda/inbox"                                                                                                                                                                                      
+  output_path = "./inbox_lambda.zip"                                                                                                                                                                         
+} 
+
+resource "aws_lambda_function" "inbox_lambda" {
+  architectures                  = ["arm64"]
+  function_name                  = "inbox_lambda"
+  handler                        = "bootstrap"
+  memory_size                    = "2048"
+  reserved_concurrent_executions = "-1"
+  role                           = aws_iam_role.api_server_role.arn
+  depends_on                     = [aws_iam_role_policy_attachment.api_server_policy_role_attachment]
+  runtime                        = "provided.al2"
+  source_code_hash               = data.archive_file.inbox_lambda_zip.output_base64sha256
+  filename                       = data.archive_file.inbox_lambda_zip.output_path 
+  timeout                        = "120"
+  publish                        = true
+  environment {
+    variables = {
+      # AWS_STS_REGIONAL_ENDPOINTS="regional",
+      # TF_LOG="trace"
+      MAIL_BUCKET="${aws_s3_bucket.mail-bucket.bucket}"
+    }
+  }
+
+  # vpc_config {
+  #   subnet_ids         = [aws_subnet.subnet_public.id]
+  #   security_group_ids = [aws_security_group.lambda_sg.id]
+  # }
+
+  # file_system_config {
+  #   arn = aws_efs_access_point.lambda_access_point.arn
+  #   local_mount_path = "/mnt/files"
+  # }
+
+  tracing_config {
+    mode = "PassThrough"
+  }
+}
+
+resource "aws_lambda_permission" "allow_ses_to_lambda" {
+    statement_id = "AllowExecutionFromSES"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.inbox_lambda.function_name
+    principal = "ses.amazonaws.com"
+    source_arn = aws_ses_receipt_rule.catch-all.arn
+}
+
+resource "aws_lambda_function_event_invoke_config" "lambda_to_ses_topic" {
+  function_name = aws_lambda_function.inbox_lambda.function_name
+  depends_on = [ aws_lambda_function.inbox_lambda ]
+}
+
+resource "aws_dynamodb_table" "example" {
+  name             = "SupermailerTable"
+  hash_key         = "PK"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
 }
